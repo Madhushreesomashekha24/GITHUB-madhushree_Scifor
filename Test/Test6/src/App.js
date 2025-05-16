b73fa97c5995ee974ebf2b1151379ddb9c4b352f
Madhushreesomashekha24 <madhushreesomashekhar@gmail.com>
2025-05-16 22:53:32 +0530

    Add files via upload

---
+++ Test/Test6/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './Welcome';
+import HelloComponent from './Hellocomponent';
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Madhu Shree" />
+      <HelloComponent />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ Test/Test6/src/dist/Hellocomponent.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _welcome = require("./welcome");
+
+var _default = _welcome.HelloComponent;
+exports["default"] = _default;
